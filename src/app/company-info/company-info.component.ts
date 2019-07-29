11406f5e8fbfe72baaf0b2518d4e1a2d635ae412
X_VZ0661@ALLYIPOC.COM <X_VZ0661@ALLYIPOC.COM>
2019-07-29 10:30:04 -0400

    getting rid of debugging comments

--- src/app/company-info/company-info.component.ts
+++ src/app/company-info/company-info.component.ts
@@ -27,13 +27,6 @@ export class CompanyInfoComponent implements OnInit {
 	
 	ngOnInit() {
 		this.getInvoices(this.route.params['value']['id']);
-		// find first created invoice date
-		// for each date from the first created invoice date:
-			// get invoice total balance on this date
-			
-		// let dataPoints = this.invoices.map(invoice => invoice.)
-		
-
 	}
 
 	private createGraph(dataPoints: any[]) {
@@ -41,36 +34,7 @@ export class CompanyInfoComponent implements OnInit {
 			return a.x.getTime() - b.x.getTime();
 		});
 		console.log("datapoints ", dataPoints);
-		// let dataPoints = this.invoices.map((invoice) => {
-		// 	console.log(invoice.balance);
-		// 	let item = { x: new Date(invoice.createdOn), y: 300 }
-		// 	// let item = { x: new Date("2018-03-01"), y: 85.3 }
-		// 	// { x: invoice.createdOn, y: 100 }
-		// 	return item;
-		// })
-		// let dataPoints = [
-			// { x: new Date("2018-03-01"), y: 85.3 },
-		// 	{ x: new Date("2018-03-02"), y: 83.97 },
-		// 	{ x: new Date("2018-03-05"), y: 83.49 },
-		// 	{ x: new Date("2018-03-06"), y: 84.16 },
-		// 	{ x: new Date("2018-03-07"), y: 84.86 },
-		// 	{ x: new Date("2018-03-08"), y: 84.97 },
-		// 	{ x: new Date("2018-03-09"), y: 85.13 },
-		// 	{ x: new Date("2018-03-12"), y: 85.71 },
-		// 	{ x: new Date("2018-03-13"), y: 84.63 },
-		// 	{ x: new Date("2018-03-14"), y: 84.17 },
-		// 	{ x: new Date("2018-03-15"), y: 85.12 },
-		// 	{ x: new Date("2018-03-16"), y: 85.86 },
-		// 	{ x: new Date("2018-03-19"), y: 85.17 },
-		// 	{ x: new Date("2018-03-20"), y: 85.99 },
-		// 	{ x: new Date("2018-03-21"), y: 86.1 },
-		// 	{ x: new Date("2018-03-22"), y: 85.33 },
-		// 	{ x: new Date("2018-03-23"), y: 84.18 },
-		// 	{ x: new Date("2018-03-26"), y: 85.21 },
-		// 	{ x: new Date("2018-03-27"), y: 85.81 },
-		// 	{ x: new Date("2018-03-28"), y: 85.56 },
-		// 	{ x: new Date("2018-03-29"), y: 88.15 }
-		// ];
+
 		let chart = new CanvasJS.Chart("chartContainer", {
 			animationEnabled: true,
 			theme: "light2",
@@ -107,14 +71,12 @@ export class CompanyInfoComponent implements OnInit {
 	}
 
 	getBalanceOnDate(balance_date: Date) : number {
-		// console.log("invoices in func: ",this.invoices);
+
 		let curBal = 0.0;
 		this.invoices.forEach(invoice => {
 			console.log("paid on ",invoice.paidOn);
 			if (!invoice.paidOn) {
-				// console.log(invoice.initialBalance);
 				curBal += invoice.initialBalance;
-				// return;
 			}
 			else if (new Date(invoice.paidOn) > balance_date) {
 				console.log("initial balance",invoice.initialBalance);
@@ -126,24 +88,12 @@ export class CompanyInfoComponent implements OnInit {
 		return curBal;
 	}
 
-	// getInvoices(id: number) {
-	// 	this.dataService.getRecords("invoice/company/" + id)
-	// 		.subscribe(
-	// 			results => this.invoices = results,
-	// 			error => this.errorMessage = <any>error);
-	// }
-  // getInvoices(id: number) {
-  //   this.dataService.getRecords("invoice/company/"+id)
-  //     .subscribe(
-  //       results => this.invoices = results,
-  //       error =>  this.errorMessage = <any>error);
-  // }
-  
+
   getInvoices(id: number) {
     this.dataService.getRecords("invoice/company/"+id)
       .subscribe((results)=>{
 		this.invoices = results; 
-		// let dates_to_check/: Date[];
+
 		let dates_to_check = new Array();
 		console.log(this.invoices);
 		this.invoices.forEach(invoice => {
@@ -159,15 +109,13 @@ export class CompanyInfoComponent implements OnInit {
 		});
 		console.log("sorted dates --> ",dates_to_check);
 		console.log("RESULTS---->",results);
-		// let minDate/ = this.getMinDate();
-		// let dataPoints: any[];
+
 		let dataPoints = new Array();
-		// let maxDate = this.getMaxDate();
+
 		dates_to_check.forEach(date => {
-			// this.getBalanceOnDate(date);
 			dataPoints.push({x:date, y:this.getBalanceOnDate(date)});
 		});
-		// this.getBalanceOnDate(new Date());
+
 		this.createGraph(dataPoints);
         return this.invoices = results;
         }, error => { this.errorMessage = <any>error});
