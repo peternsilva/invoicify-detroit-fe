f79b97392fe9b90628559e2988bb1c162853fce8
Caleb Richter <calebric@umich.edu>
2019-07-30 16:28:36 -0400

    workin on analytics income line graph

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -40,7 +40,7 @@ export class AnalyticsComponent implements OnInit {
       animationEnabled: true,
       theme: "light2",
       title: {
-        text: "Balance History"
+        text: "Balance History (YTD)"
       },
       axisX: {
         valueFormatString: "DD MMM YYYY",
@@ -71,6 +71,46 @@ export class AnalyticsComponent implements OnInit {
     chart.render();
   }
 
+  private create30DayLineGraph(dataPoints: any[], chartName: String) {
+    dataPoints.sort((a, b) => {
+      return a.x.getTime() - b.x.getTime();
+    });
+    console.log("datapoints ", dataPoints);
+
+    let chart = new CanvasJS.Chart(chartName, {
+      animationEnabled: true,
+      theme: "light2",
+      title: {
+        text: "Balance History (30 day)"
+      },
+      axisX: {
+        valueFormatString: "DD MMM YYYY",
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true
+        }
+      },
+      axisY: {
+        title: "Balance (in USD)",
+        includeZero: false,
+        valueFormatString: "$##0.00",
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true,
+          labelFormatter: function (e) {
+            return "$" + CanvasJS.formatNumber(e.value, "##0.00");
+          }
+        }
+      },
+      data: [{
+        type: "area",
+        xValueFormatString: "DD MMM YYYY",
+        yValueFormatString: "$##0.00",
+        dataPoints: dataPoints
+      }]
+    });
+    chart.render();
+  }
   getBalanceOnDate(balance_date: Date): number {
 
     let curBal = 0.0;
@@ -94,7 +134,7 @@ export class AnalyticsComponent implements OnInit {
     this.invoices.forEach(invoice => {
       console.log("paid on ", invoice.paidOn);
       if (!invoice.paidOn) {
-        curBal += invoice.initialBalance;
+        return;
       }
       else if (new Date(invoice.paidOn) > balance_date) {
         console.log("initial balance", invoice.initialBalance);
@@ -145,16 +185,22 @@ export class AnalyticsComponent implements OnInit {
     // console.log("RESULTS---->", results);
     let dataPoints = new Array();
     let dataPoints30day = new Array();
+    let dataPointsIncome = new Array();
     dates_to_check.forEach(date => {
       let bal_on_date = this.getBalanceOnDate(date)
       if (date.getFullYear() === 2019)
         dataPoints.push({ x: date, y: bal_on_date });
       if (Date.now() - date.getTime() < 30 * 8.64e+7)
         dataPoints30day.push({ x: date, y: bal_on_date });
+      if (date.getFullYear() === 2019) {
+        let income_on_date = this.getIncomeOnDate(date);
+        dataPointsIncome.push({x: date, y: income_on_date});
+      }
+
     });
     this.createLineGraph(dataPoints, "chartContainer");
-    this.createLineGraph(dataPoints30day, "30dayChart");
-
+    this.create30DayLineGraph(dataPoints30day, "30dayChart");
+    this.createLineGraph(dataPointsIncome, "incomeYTD");
   }
 
   private setDataCards() {
