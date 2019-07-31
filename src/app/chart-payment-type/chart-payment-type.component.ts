536b3feb9b6097b7d9c1fcf8bb2c5c9941d90691
Caleb Richter <calebric@umich.edu>
2019-07-31 15:10:34 -0400

    implemented payment type chart

--- src/app/chart-payment-type/chart-payment-type.component.ts
+++ src/app/chart-payment-type/chart-payment-type.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../data.service'
+import * as CanvasJS from '../../assets/canvasjs.min';
+
 @Component({
   selector: 'app-chart-payment-type',
   templateUrl: './chart-payment-type.component.html',
@@ -7,9 +10,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartPaymentTypeComponent implements OnInit {
 
-  constructor() { }
+  
+  errorMessage: string;
+  successMessage: string;
+  invoices: any[];
+  dataCards: any;
+  title = 'canvasjs-angular';
+
+  constructor(
+    private dataService: DataService,
+  ) { }
 
   ngOnInit() {
+    this.getDataCards();
   }
+  private createDonutGraph(dataPoints: any[], chartName: String, color: String, textVal: String) {
+
+    console.log("datapoints ", dataPoints);
+    let defColor : String;
+    if (color === "red"){
+        defColor = "#FF5733";
+    }
+    else if (color === "green"){
+      defColor = "#33FF5F";
+    }
+    else if (color === "blue"){
+      defColor = "#33C5FF";
+    }
+    let chart = new CanvasJS.Chart(chartName, {
+      animationEnabled: true,
+      theme: "light2",
+      title: {
+        text: textVal
+      },
+      axisX: {
+        valueFormatString: "DD MMM YYYY",
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true
+        }
+      },
+      axisY: {
+        title: "Balance (in USD)",
+        includeZero: false,
+        valueFormatString: "$##0.00",
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true,
+          labelFormatter: function (e) {
+            return "$" + CanvasJS.formatNumber(e.value, "##0.00");
+          }
+        }
+      },
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: dataPoints
+      }]
+    });
+    chart.render();
+  }
+
+  getDataCards() {
+    this.dataService.getRecords("data/")
+      .subscribe((results) => {
+        this.dataCards = results;
+
+        this.setDataCards();
+        return this.dataCards = results;
+      }, error => { this.errorMessage = <any>error });
+
+  }
+  setDataCards() {
+    let dataPoints = new Array();
+    this.dataCards.findByRecordType.forEach(paymentMeth => {
+      if (paymentMeth[1] === "FlatFeeBillingRecord")
+        paymentMeth[1] = "Flat Fee"
+      else
+        paymentMeth[1] = "Rate Based"
+      dataPoints.push({ y: paymentMeth[0], label:paymentMeth[1]})
+    });
+    console.log("dataPoints----",dataPoints);
+    this.createDonutGraph(dataPoints, "paymentMethChart","blue", "Payment Method");
+
+  }
+
+
 
 }
